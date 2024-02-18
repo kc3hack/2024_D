b9df16b4481b576130eb04deea727b2949b3b99e
Lamy210 <syoytaku@gmail.com>
2024-02-18 06:35:04 +0000

    create springboot

---
+++ Backend/store_api/src/test/java/com/example/store_api/StoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.store_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
