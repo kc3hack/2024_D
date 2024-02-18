51a88e6d00df36419b2888bc429072e5bd276419
Lamy210 <syoytaku@gmail.com>
2024-02-18 16:30:10 +0000

    パラメータ設置

--- randommapapi/src/main/java/com/example/randommapapi/Controller/RandomStoreController.java
+++ randommapapi/src/main/java/com/example/randommapapi/Controller/RandomStoreController.java
@@ -1,6 +1,7 @@
 package com.example.randommapapi.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping; 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController; 
 
 @RestController           
@@ -8,11 +9,26 @@ public class RandomStoreController {
 
     @GetMapping("/hello")  
     public String sayHello() { 
-        
+
 
 
 
 
         return "Hello, world!"; 
     } 
+    @GetMapping("/test")
+    public String getStores(
+        @RequestParam(name = "latitude") float latitude,
+        @RequestParam(name = "longitude") float longitude,
+        @RequestParam(name = "searchRadius") int searchRadius,
+        @RequestParam(name = "type") String type) {
+
+        // ここに処理を書く
+
+        return "OK";
+    }
+
+
+
+
 }
