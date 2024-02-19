55d2ebddd1e26478340b39ed810ffb0e590641a4
Lamy210 <syoytaku@gmail.com>
2024-02-19 15:24:21 +0000

    Controler

--- randommapapi/src/main/java/com/example/randommapapi/Controller/RandomStoreController.java
+++ randommapapi/src/main/java/com/example/randommapapi/Controller/RandomStoreController.java
@@ -3,6 +3,10 @@ package com.example.randommapapi.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController; 
+import org.springframework.web.bind.annotation.PostMapping; // Import the PostMapping annotation
+import org.springframework.http.ResponseEntity; // Import the ResponseEntity class
+import com.example.randommapapi.Model.MyModel; // Import the MyModel class
+import org.springframework.web.bind.annotation.RequestBody; // Import the RequestBody class
 
 @RestController           
 public class RandomStoreController { 
@@ -22,7 +26,7 @@ public class RandomStoreController {
 
         // ここに処理を書く
 
-        return "OK";
+        return ResponseEntity.ok(model);
     }
 
 
